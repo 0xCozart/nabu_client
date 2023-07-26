60a76efd13e999f791fb2df88919d2aab323472f
alan <alanvega002@gmail.com>
2023-07-25 18:16:05 -0700

    using zod for env vars

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,8 @@
-import dotenv from "dotenv";
 import path, { dirname } from "path";
+
+import dotenv from "dotenv";
 import { fileURLToPath } from "url";
+import { z } from "zod";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,6 +11,17 @@ const __dirname = dirname(__filename);
 dotenv.config({
   path: path.resolve(__dirname, "../..", ".env"),
 });
+
+const envSchema = z.object({
+  NEXT_PUBLIC_DID: z.string().nonempty(),
+  NEXT_PUBLIC_ED25519_SEED: z.string().nonempty(),
+  NEXT_PUBLIC_LAVA_BADGE_ADDRESS: z.string().nonempty(),
+  NEXT_PUBLIC_LAVA_PROJECT_ID: z.string().nonempty(),
+});
+
+const env = envSchema.parse(process.env);
+
+export { env };
 
 const constants = {
   publicDID: process.env.NEXT_PUBLIC_DID || "",
