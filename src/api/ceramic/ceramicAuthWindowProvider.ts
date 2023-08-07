630cd71d860d026ebc0a5f21173eb2b23f3514e0
alan <alanvega002@gmail.com>
2023-08-07 16:37:05 -0700

    connect to injected windows provider

--- src/api/ceramic/ceramicAuthWindowProvider.ts
+++ src/api/ceramic/ceramicAuthWindowProvider.ts
@@ -4,78 +4,9 @@
 import { ethers } from "ethers";
 
 export const CeramicAuthWindowProvider = async () => {
-  // eslint-disable-next-line
-  const ethProvider = await new ethers.providers.Web3Provider(window.ethereum);
-  // console.log({ ethProvider });
-  const signer = ethProvider.getSigner();
-  console.log({ signer: signer.getAddress() });
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  await provider.send("eth_requestAccounts", []);
+
+  const signer = provider.getSigner();
+  return { provider, signer };
 };
-
-
-// export const CeramicAuth = async () => {
-// eslint-disable-next-line
-// const lavasdk = await new sdk.LavaSDK({
-//   badge: {
-//     badgeServerAddress: env.NEXT_PUBLIC_LAVA_BADGE_ADDRESS,
-//     projectId: env.NEXT_PUBLIC_LAVA_PROJECT_ID,
-//   },
-//   chainID: "ETH1",
-//   rpcInterface: "jsonrpc",
-//   geolocation: "2",
-// });
-
-// const addresses = await lavasdk.sendRelay({
-//   method: "eth_accounts",
-//   params: [],
-// });
-
-// const accountID = await getAccountId(ethProvider, addresses[0]);
-// console.log({ addresses });
-
-//   const authenticateDID = async (seed: string) => {
-//     try {
-//       const Uint8ArraySeed = seedToUint8Array(seed);
-//       const provider = new Ed25519Provider(Uint8ArraySeed);
-//       const did = new DID({ provider, resolver: getResolver() });
-//       await did.authenticate();
-//       return did;
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
-//   const did = await authenticateDID(constants.seed);
-//   if (!did) throw new Error("did is undefined");
-//   console.log({ id: did.id });
-//   return did;
-// await client
-//   .mutate({
-//     mutation: gql`
-//       mutation createNabuProfileAlpha($i: CreateNabuProfileAlphaInput!) {
-//         createNabuProfileAlpha(input: $i) {
-//           document {
-//             id
-//             userDID {
-//               id
-//             }
-//             displayName
-//             accountCreated
-//           }
-//         }
-//       }
-//     `,
-//     variables: {
-//       i: {
-//         content: {
-//           userDID: "did:key:z6MkjfFcCdV5ph6i4BUSrb2gsq6ZuhEr7Vpff2TioLUYVSiD",
-//           displayName: "testName1",
-//           accountCreated: new Date().toISOString(),
-//         },
-//       },
-//     },
-//   })
-//   .then((value) => {
-//     console.log({ value });
-//   });
-// };
-
-// void CeramicAuth();
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,6 @@
-import { useState, type MouseEvent } from "react";
-
-import Image from "next/image";
 import { CeramicAuthWindowProvider } from "../api/ceramic/ceramicAuthWindowProvider";
+import Image from "next/image";
+import { useState } from "react";
 
 const Navbar = () => {
   const [signer, setSigner] = useState<unknown>();
@@ -11,9 +10,8 @@ const Navbar = () => {
     setSigner(signer);
   };
 
-  const handleClick = async (e: MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    await executeAuth();
+  const handleClick = () => {
+    void executeAuth();
   };
 
   return (
@@ -36,7 +34,7 @@ const Navbar = () => {
             <button
               type="button"
               className="mr-3 rounded-lg bg-blue-700 px-4 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 md:mr-0"
-              onClick={void handleClick}
+              onClick={handleClick}
             >
               Auth
             </button>
